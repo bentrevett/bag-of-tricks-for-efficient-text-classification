f1ae251cbff2102c817a9e8e95ccd0a7d58571e9
bentrevett <bentrevett@gmail.com>
2018-03-06 19:20:25 +0000

    added comments to the model

--- models.py
+++ models.py
@@ -9,12 +9,23 @@ class FastText(nn.Module):
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
 
+        #maps sparse vector inputs to dense vector word embeddings
         self.embedding = nn.Embedding(input_dim, hidden_dim)
+
+        #goes from the average of the word embeddings to the output prediction
         self.fc = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
+        #x = [batch size, seq. length]
+        
 
         x = self.embedding(x)
-        x = F.avg_pool2d(x, (x.shape[1], 1)).squeeze(1)
+        #x = [batch size, seq. length, hidden dim]
+        
+        x = F.avg_pool2d(x, (x.shape[1], 1)).squeeze(1) #averages word vectors across whole sequence length
+        #x = [batch size, hidden dim]
+
         x = self.fc(x)
+        #x = [batch size, output dim]
+        
         return x
